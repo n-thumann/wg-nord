cc98df10b70245c50c8ec8f82dbe1ef374612c44
Nicolas Thumann <me@n-thumann.de>
2024-06-09 01:36:17 +0200

    Add: NordVPN API

---
+++ src/nordvpn/api/credentials.rs
@@ -0,0 +1,33 @@
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub struct Credentials {
+    pub nordlynx_private_key: Option<String>,
+    errors: Option<Error>,
+}
+
+#[derive(Deserialize, Debug)]
+struct Error {
+    message: String,
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "NordVPN API error: {}", self.message)
+    }
+}
+impl std::error::Error for Error {}
+
+pub fn get(token: String) -> Result<Credentials, Box<dyn std::error::Error>> {
+    let client = reqwest::blocking::Client::new();
+    let body: Credentials = client
+        .get("https://api.nordvpn.com/v1/users/services/credentials")
+        .header("Authorization", format!("token:{}", token))
+        .send()?
+        .json()?;
+
+    match body.errors {
+        Some(x) => Err(Error { message: x.message }.into()),
+        None => Ok(body),
+    }
+}
---
+++ src/nordvpn/api/servers.rs
@@ -0,0 +1,40 @@
+use std::{error::Error, net::IpAddr};
+
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub struct IP {
+    pub ip: IpAddr,
+    pub version: u64,
+}
+
+#[derive(Deserialize)]
+pub struct IPContainer {
+    pub ip: IP,
+}
+
+#[derive(Deserialize)]
+pub struct Metadata {
+    pub name: String,
+    pub value: String,
+}
+
+#[derive(Deserialize)]
+pub struct Technology {
+    pub identifier: String,
+    pub metadata: Vec<Metadata>,
+}
+
+#[derive(Deserialize)]
+pub struct Server {
+    pub hostname: String,
+    pub ips: Vec<IPContainer>,
+    pub technologies: Vec<Technology>,
+}
+
+pub fn get() -> Result<Vec<Server>, Box<dyn Error>> {
+    let body: Vec<Server> = reqwest::blocking::get("https://api.nordvpn.com/v1/servers?fields[servers.technologies]&fields[servers.hostname]&fields[servers.ips]&limit=0")?
+        .json()?;
+
+    Ok(body)
+}
